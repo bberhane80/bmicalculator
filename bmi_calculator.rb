dda339145f2ec421217ce8c0e98fb8dca44709f9
Berhane Berhane <berhane896@gmail.com>
2025-03-31 18:23:19 +0000

    added main file, defined a BMI class, and added necessary methods

---
+++ bmi_calculator.rb
@@ -0,0 +1,34 @@
+attr_accessor :height, :weight
+
+def initialize
+  puts "Welcome to Your BMI Calculator"
+end
+
+def calculate
+  puts "Enter your height in meters: "
+  @height = gets.chomp.to_f
+
+  puts "Enter your weight in kg: "
+
+  bmi = (@weight / (@height ** 2)).round(1)
+  puts "Your BMI is #{bmi}" - #{category(bmi)}"
+end 
+
+private
+
+def category(bmi)
+
+  if bmi < 18.5
+    print "Underweight"
+  elsif bmi < 25
+    print "Normal"
+  elsif bmi < 30
+    print "Overweight"
+  else
+    print "Obese"
+    end
+  end
+end
+
+BMI.new.calculate
+
